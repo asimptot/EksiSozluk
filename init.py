6da5e260870f0203e600c08793bb2618cf0d20f4
Asım <asimzorlu@gmail.com>
2025-01-03 16:25:27 +0100

    Add files via upload

--- init.py
+++ init.py
@@ -1,14 +1,17 @@
 from time import sleep
 import warnings
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import undetected_chromedriver as uc
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 class Setup:
-    def init(self):
+    def __init__(self):
         options = uc.ChromeOptions()
         options.add_argument('--headless')
         options.add_argument("--window-position=-2400,-2400")
@@ -17,15 +20,14 @@ class Setup:
         options.add_argument(
             "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
         options.add_argument("--disable-blink-features=AutomationControlled")
-
         options.add_argument("--disable-extensions")
         options.add_argument("--disable-popup-blocking")
         options.add_argument("--disable-infobars")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
 
-        chrome_binary_path = "/opt/render/.local/share/pyppeteer/local-chromium/588429/chrome-linux/chrome"  # Chrome'un yüklü olduğu yol
-        self.browser = uc.Chrome(options=options, browser_executable_path=chrome_binary_path)
+        service = Service(ChromeDriverManager().install())
+        self.browser = webdriver.Chrome(service=service, options=options)
 
         # WebDriver algılamalarını engelle
         self.browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
